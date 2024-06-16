28e3f9eed963b37c36d45364a9febb3056a8b6ae
JuniorFixHow <robertjuniorannan@gmail.com>
2024-06-16 04:16:02 +0100

    Question Two

--- QTwo/Program.cs
+++ QTwo/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace QTwo
+﻿public class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+   static void Main(string[] args)
+   {
+        //take the radius of a circle from user
+        Console.Write("\t\tPlease enter the radius of a circle\n");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        //take the length of a rectangle from user
+        Console.Write("\t\tPlease enter the length of a rectangle\n");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+
+        //take the width of a rectangle from user
+        Console.Write("\t\tPlease enter the width of a rectangle\n");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        //call classes and create instances
+        Shape circle = new Circle(radius);
+        Shape rectangle = new Rectangle(length, width);
+
+        //print results
+        Console.WriteLine($"\n\t\tArea of the circle: {circle.getArea()} squared units");
+        Console.WriteLine($"\n\t\tArea of the rectangle: {rectangle.getArea()} squared units");
     }
 }
---
+++ QTwo/Shape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//abstract shape class
+public abstract class Shape
+ {
+    public abstract double getArea();
+ }
+
+
+//Derived Circle class
+public class Circle : Shape
+{
+    private double radius;
+
+    //Contructor method;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    //override the abstract method
+    public override double getArea()
+    {
+        //return area of a curcle
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    //dimensions of a rectangle
+    private double length;
+    public double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    //override the abstract method
+    public override double getArea()
+    {
+        //return the area of a circle
+        return length * width;
+    }
+}
