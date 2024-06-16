46d8c186b86025e1761bc63420a4fe003524f508
JuniorFixHow <robertjuniorannan@gmail.com>
2024-06-16 02:47:59 +0100

    Question One

---
+++ QOne/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+  public class Animal
+  {
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("\t\tSome generic sound");
+    }
+  }
+
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("\t\tBark");
+    }
+}
+
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("\t\tMeow");
+    }
+}
---
+++ QOne/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tQuestion One\n");
+
+            //Call classes
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            animal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
