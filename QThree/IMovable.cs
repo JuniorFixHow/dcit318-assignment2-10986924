f61dc18824427cf8a91dfcad4887e3acbf3a7733
JuniorFixHow <robertjuniorannan@gmail.com>
2024-06-16 04:40:58 +0100

    Question Three

---
+++ QThree/IMovable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//define new interface
+public interface IMovable
+{
+    void Move();
+}
+
+//Car class that is extending the IMovable interface
+public class Car : IMovable
+{
+    //a method that is implementing the Move method in IMovable
+    public void Move()
+    {
+        Console.WriteLine("\t\t\tCar is moving\n");
+    }
+}
+
+
+//Bicycle class that is extending the IMovable interface
+public class Bicycle : IMovable
+{
+    //a method that is implementing the Move method in IMovable
+    public void Move()
+    {
+        Console.WriteLine("\t\t\tBicycle is moving");
+    }
+}
---
+++ QThree/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+   static void Main(string[] args)
+   {
+      Console.WriteLine("\t\t\t>>>Question Three<<<\n\n\n");
+
+        //call classes and instances
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        //call methods
+        car.Move();
+        bicycle.Move();
+   }
+}
